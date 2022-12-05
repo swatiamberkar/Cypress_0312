baf70d089bc8f51c1ccbe23cba3c330d89d65baa
swati <swatiamberkar29@gmail.com>
2022-12-05 17:23:02 +0530

    add module wise files

---
+++ cypress/integration/Employee/01_Login.js
@@ -0,0 +1,17 @@
+describe('Login <home>', function() {
+	
+	beforeEach(function(){
+        window.console.log('Enter the beforeEach function')
+		Cypress.on('uncaught:exception', (err,runnable) => {
+				return false;
+		});
+		Cypress.Cookies.preserveOnce('.AspNetCore.Antiforgery.w5W7x28NAIs','module','__cypress.initial','.AspNetCore.Session','1P_JAR','ARRAffinity','FavouriteMenus','XCategory','XCompanyId','XName','XSchemaName','XUserId','XUserName','_ga','_gid','ai_session','ai_user','new_username','GetLicenseData')
+		cy.wait(2000)
+    })
+	
+	it('successfully loads', function() {
+		cy.visit('http://next.pockethrms.com/') 
+	})
+	
+	
+	})
